777e6d74b079576ea334d369b222766c1ab51200
TEROX <shahidtashildar@gmail.com>
2024-07-21 18:33:45 +0530

    added stop button

--- sound.py
+++ sound.py
@@ -9,9 +9,13 @@ def play():
     pygame.mixer.music.load("F:/MUSIC/afgaan.mp3")
     pygame.mixer.music.play(loops=0)
 
+def stop():
+    pygame.mixer.music.stop()
 
 play_button = Button(root, text="Play song" ,font=("Helvetica", 32), command=play)
 play_button.pack(pady=20)
 
+stop_button = Button(root, text="Stop" ,font=("Helvetica", 32), command=stop)
+stop_button.pack(pady=20)
 
 root.mainloop()
